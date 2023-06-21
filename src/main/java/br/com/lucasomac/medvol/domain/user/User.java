8ca7d0c84c8d27a6b3ea6ec5339ae77df5ad7ed5
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2023-06-20 23:16:52 -0300

    feature(security): Implemented basic security for spring

---
+++ src/main/java/br/com/lucasomac/medvol/domain/user/User.java
@@ -0,0 +1,39 @@
+package br.com.lucasomac.medvol.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Table(name = "user")
+@Entity(name = "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String password;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ src/main/resources/db/migration/V5__create-table-user.sql
@@ -0,0 +1,6 @@
+-- CREATE TABLE user
+-- (
+--     id       BIGINT       NOT NULL PRIMARY KEY,
+--     login    VARCHAR(100) NOT NULL,
+--     password VARCHAR(255) NOT NULL
+-- );
